f5883889d8e86f04d457e5e6988afc9cafd3640e
Eric Still <58347689+Eric02851@users.noreply.github.com>
2023-02-27 00:42:36 -0800

    Working GPU screen capture
    Almost 100% success rate. Could be improved with smaller screen capture regions.

--- main.py
+++ main.py
@@ -1,54 +1,67 @@
 import time
 import pydirectinput
-import pyautogui
+import dxcam
 
 class Circle:
     white = (255,255,255)
     blue = (125, 205, 255)
-    x = 1280
-    y = 1335
-    bottomy = 1385
 
-class fishingBar:
+    middley = 1271
+    bottomy = 1333
+    x = 1280
+
+class Bar:
     blue = (52, 201, 255)
     yellow = (255, 206, 28)
-    cords = (1075, 1179, 410, 1)
+
+    width = 410
+    x = 1075
+    y = 1134
+
+def getFrame():
+    frame = cam.grab()
+    while frame is None: frame = cam.grab()
+    return frame
 
 def checkBar():
-    img = pyautogui.screenshot(region=fishingBar.cords)
+    frame = getFrame()
+    bluePixels = set()
 
-    bluePixels = []
     for i in range(410):
-        color = img.getpixel((i,0))
-        if color == fishingBar.blue:
-            bluePixels.append(i)
+        color = tuple(frame[Bar.y, Bar.x + i])
+        if color == Bar.blue: bluePixels.add(i)
 
-    #bluePixels = bluePixels[int(len(bluePixels)/2) -2:int(len(bluePixels)/2) +2]
     yellowOnBlue=False
     while not yellowOnBlue:
-        img = pyautogui.screenshot(region=fishingBar.cords)
+        frame = getFrame()
         for i in bluePixels:
-            if img.getpixel((i,0)) == fishingBar.yellow:
+            if tuple(frame[Bar.y, Bar.x + i]) == Bar.yellow:
                 pydirectinput.click(Circle.x, Circle.bottomy)
                 yellowOnBlue = True
+
+                print(F"Detected: {Bar.x + i}")
                 break
 
-def checkfishon():
-    pixelwhite = pyautogui.pixelMatchesColor(Circle.x, Circle.y, Circle.white)
-    pixelblue = pyautogui.pixelMatchesColor(Circle.x, Circle.y, Circle.blue)
-    return pixelwhite or pixelblue
+def checkFishing():
+    frame = getFrame()
+    color = tuple(frame[Circle.middley, Circle.x])
+    return color == Circle.white or color == Circle.blue
 
+cam = dxcam.create()
+fishCount = 0
 time.sleep(3)
+
 while True:
     pydirectinput.click(1280, 720)
+    while not checkFishing(): time.sleep(0.1)
 
-    while not checkfishon():
-        time.sleep(0.1)
-
-    while checkfishon():
+    while checkFishing():
         checkBar()
         time.sleep(0.1)
 
     time.sleep(0.5)
     pydirectinput.press('e')
     time.sleep(0.5)
+
+    fishCount += 1
+    print(f"Caught Fish: {fishCount}\n")
